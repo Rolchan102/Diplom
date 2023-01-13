6c6e8e6039425d9ca907e541cb54cb8bd84e8698
Rolchan102 <106190890+Rolchan102@users.noreply.github.com>
2023-01-13 19:17:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,138 +1,119 @@
-import time
-import csv
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium_stealth import stealth
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from openpyxl import Workbook
+from time import sleep
+
+# geckodriver_path = r'C:/Users/ktoto/Documents/geckodriver.exe' #
+chromedriver_path = r''
+url = 'https://pa.drweb.com/static/web_price/web/'    
 
 
-def get_data_with_selenium(url):
-    global price_per_unit
-    options = webdriver.ChromeOptions()
-    options.add_argument("start-maximized")
+class Parser:
+    def __init__(self):
+        self.wb = Workbook()  # для экселя
+        self.ws = self.wb.active
+        
+        # self.driver = webdriver.Firefox(executable_path=geckodriver_path)
+        self.driver = webdriver.Chrome(executable_path=chromedriver_path)
+        self.driver.implicitly_wait(5)
+        self.driver.get(url)
+        sleep(5)
+        select = Select(self.driver.find_element(By.XPATH, '//*[@id="package_select"]'))
+        select.select_by_visible_text('Россия')
+        select = Select(self.driver.find_element(By.XPATH, '//*[@id="discount_type"]'))
+        select.select_by_visible_text('Новая лицензия')
+        for i in ['12', '24', '36']:
+            select = Select(self.driver.find_element(By.XPATH, '//*[@id="period_license"]'))
+            select.select_by_visible_text(i)
+            sleep(1)
+            all_items = self.driver.find_elements(By.CLASS_NAME, 'additional')
+            for item in all_items:
+                checkboxes_cells = item.find_elements(By.CLASS_NAME, 'cell')
+                self.get_all_info(item, checkboxes_cells[0])
+        self.driver.quit()
+        self.wb.save("result.xlsx")
 
-    # options.add_argument("--headless")
+    def get_all_info(self, item, checkboxes_cell):
+        checkboxes = checkboxes_cell.find_elements(By.TAG_NAME, 'label')
+        for checkbox in checkboxes:
+            if 'Сертифицированный ФСТЭК' in checkbox.get_attribute('innerHTML'):
+                checkboxes.remove(checkbox)
+                break
+        checkbox_count = len(checkboxes)
+        checkboxes_check = checkboxes_cell.find_elements(By.TAG_NAME, 'input')
+        flag, input_, value_0, value_1 = self.write_count(item)
+        sleep(0.5)
+        self.get_info()
+        sleep(0.5)
+        if checkbox_count == 1:
+            self.click_one_checkbox(checkboxes_check, 0)
+        elif checkbox_count == 2:
+            self.click_two_checkboxes(checkboxes_check, 0, 1)
+        elif checkbox_count == 3:
+            self.click_two_checkboxes(checkboxes_check, 0, 1)
+            self.click_one_checkbox(checkboxes_check, 1)
+            sleep(0.5)
+            self.click_one_checkbox(checkboxes_check, 2)
+            sleep(0.5)
+            self.get_info()
+            self.click_two_checkboxes(checkboxes_check, 0, 1)
+        self.clear_count(flag, input_, value_0, value_1)
 
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
-    s = Service("C:\chromedriver.exe")
-    driver = webdriver.Chrome(
-        service=s,
-        options=options)
+    def click_two_checkboxes(self, checkboxes_check, i, j):
+        self.click_one_checkbox(checkboxes_check, i)  # + -
+        self.click_one_checkbox(checkboxes_check, j)  # + +
+        checkboxes_check[i].click()              # - +
+        sleep(0.5)
+        self.get_info()
+        sleep(0.5)
+    
+    def click_one_checkbox(self, checkboxes_check, i):
+        checkboxes_check[i].click()
+        sleep(0.5)
+        self.get_info()
+        sleep(0.5)
 
-    stealth(driver,
-            languages=["ru-RU", "ru"],
-            vendor="Google Inc.",
-            platform="Win32",
-            webgl_vendor="Intel Inc.",
-            renderer="Intel Iris OpenGL Engine",
-            fix_hairline=True,
-            )
-
-    url = "file:///E:/Pyton/PycharmProjects/Selenium-stealth/price/index.html"
-    driver.get(url)
-    # Выбрать прайс-лист: Россия
-    select = Select(driver.find_element(By.XPATH, '//*[@id="package_select"]'))
-    select.select_by_visible_text('Россия')
-
-    # Срок лицензии
-    select = Select(driver.find_element(By.XPATH, '//*[@id="period_license"]'))
-    select.select_by_visible_text('12')
-
-    # Льгота
-    select = Select(driver.find_element(By.XPATH, '//*[@id="discount_type"]'))
-    select.select_by_visible_text('Новая лицензия')
-
-    # Количество
-    # for i in range(5, 6):
-    #     text_input = driver.find_element(By.XPATH, '//*[@id="business_dwdsscp"]')
-    #     # Удалить предыдущий ввод
-    #     text_input.clear()
-    #     text_input.send_keys(i)
-    i = 1
-
-    # Dr.Web Desktop Security Suite
-    dss_input = driver.find_element(By.XPATH, '//*[@id="business_dwdsscp"]')
-    dss_input.send_keys(5)
-
-    # Dr.Web Server Security Suite
-    dss_input = driver.find_element(By.XPATH, '//*[@id="business_dwsss"]')
-    dss_input.send_keys(i)
-
-    # Dr.Web Mail Security Suite
-    dss_input = driver.find_element(By.XPATH, '//*[@id="business_dwmss"]')
-    dss_input.send_keys(i)
-
-    # Dr.Web Gateway Security Suite
-    dss_input = driver.find_element(By.XPATH, '//*[@id="business_dwgss"]')
-    dss_input.send_keys(i)
-
-    # Dr.Web Mobile Security Suite
-    dss_input = driver.find_element(By.XPATH, '//*[@id="business_dwmoss"]')
-    dss_input.send_keys(i)
-
-    # Dr.Web Katana Windows
-    dss_input = driver.find_element(By.XPATH, '//*[@id="business_dwk"]')
-    dss_input.send_keys(i)
-
-    # Dr.Web Katana Windows Server
-    dss_input = driver.find_element(By.XPATH, '//*[@id="business_dwsssk"]')
-    dss_input.send_keys(i)
-
-    # Dr.Web ATM Shield
-    dss_input = driver.find_element(By.XPATH, '//*[@id="drw_atm_shield_biz"]')
-    dss_input.send_keys(i)
-
-    with open("index_selenium.html", "w", encoding='utf-8') as file:
-        file.write(driver.page_source)
-
-    time.sleep(5)
-    driver.quit()
-
-    with open("index_selenium.html", encoding='utf-8') as file:
-        src = file.read()
-
-    soup = BeautifulSoup(src, "lxml")
-
-    l_float = soup.find("div", {"id": "basket"}).find("div", {"name": "item_of_basket_products"}).find_all("div", class_="l-float")
-    print(l_float)
-    r_float = soup.find("div", {"id": "basket"}).find("div", {"name": "item_of_basket_products"}).find_all("div", class_="r-float")
-    print(r_float)
-
-    for item in l_float:
+    def write_count(self, item):
+        flag = False
+        lpad = item.find_element(By.CLASS_NAME, 'lpad')
         try:
-            name = item.find("h6", class_="title").string
+            input_ = lpad.find_element(By.TAG_NAME, 'input')
         except:
-            name = "Нет наименования позиции"
-        try:
-            product_code = item.find("small").string
-        except:
-            product_code = "Нет кода продукта"
-        try:
-            price_per_unit = item.find("span", class_="gray").string
-        except:
-            price_per_unit = "Нет цены за позицию"
+            select_input_ = lpad.find_element(By.TAG_NAME, 'select')
+            flag = True
+        if flag:
+            select_input = Select(select_input_)
+            select_options = select_input_.find_elements(By.TAG_NAME, 'option')
+            value_0 = select_options[0].get_attribute('value')
+            value_1 = select_options[1].get_attribute('value')
+            select_input.select_by_value(value_1)
+            input_ = select_input
+        else:
+            value_0, value_1 = None, None
+            minimum_ = lpad.find_element(By.CLASS_NAME, 'hint')
+            minimum = int(minimum_.get_attribute('min'))
+            input_.send_keys(minimum)
+        return flag, input_, value_0, value_1
 
-    for total_price in r_float:
-        try:
-            total_price = total_price.find("span").string
-        except:
-            total_price = "Нет итоговой цены"
+    def get_info(self):
+            panel = self.driver.find_element(By.XPATH, '/html/body/div[3]/div[4]/div[2]/div[2]/div/div[1]/div/div[1]')
+            title = panel.find_element(By.CLASS_NAME, 'title').get_attribute('innerHTML').strip()
+            grays = panel.find_elements(By.CLASS_NAME, 'gray')
+            tag = grays[0].get_attribute('innerHTML').strip()
+            price = grays[-1].get_attribute('innerHTML').strip(r' /.-')
+            self.ws.append([title, tag, price])
 
-    print(product_code, ",", name, ",", price_per_unit, ",", total_price)
+    def clear_count(self, flag, input_, value_0, value_1):
+        if flag:
+            input_.select_by_value(value_0)
+        else:
+            input_.clear()
 
-    with open("Dr.web_price.csv", "a", encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(
-            [product_code, name, price_per_unit, total_price]
-        )
-
-
-def main():
-    get_data_with_selenium("file:///E:/Pyton/PycharmProjects/Selenium-stealth/price/index.html")
+    def write_data_txt(self, title, tag, price):
+        with open('output.txt', 'a') as f:
+            f.write(f'{title}\n{tag}\n{price}\n____________________________\n')
 
 
 if __name__ == '__main__':
-    main()
+    p = Parser()
